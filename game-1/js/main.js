591d2205a797c6189e6c1efc422cd0f907464ac0
jeremyfm4 <57880870+jeremyfm4@users.noreply.github.com>
2020-02-13 01:10:31 -0500

    Update main.js

--- game-1/js/main.js
+++ game-1/js/main.js
@@ -1,48 +1,49 @@
-"use strict";
-
-window.onload = function() {
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
-    
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
-    
-    function preload() {
+ // Create the state that will contain the whole game
+var mainState = {  
+    preload: function() {  
+        // Here we preload the assets
         game.load.image('player', 'assets/player.png');
-        game.load.image('wall', 'assets/wall.png');
-        game.load.image('coin', 'assets/coin.png');
-        game.load.image('enemy', 'assets/enemy.png');
-    }
-    
-    var bouncy;
-    
-    function create() {
-        game.stage.backgroundColor = '#3598db';
-        game.physics.startSystem(Phaser.Physics.ARCADE);
-        game.world.enableBody = true;
-        this.cursor = game.input.keyboard.createCursorKeys();
-        this.player = game.add.sprite(70, 100, 'player');
-        this.player.body.gravity.y = 600;
-        this.walls = game.add.group();
-        this.coins = game.add.group();
-        this.enemies = game.add.group();
-        var level = [
-        'xxxxxxxxxxxxxxxxxxxxxx',
-        '!         !          x',
-        '!                 o  x',
-        '!         o          x',
-        '!                    x',
-        '!     o   !    x     x',
-        'xxxxxxxxxxxxxxxx!!!!!x',
-        ];
-        // Create the level by going through the array
-        for (var i = 0; i < level.length; i++) {
-            for (var j = 0; j < level[i].length; j++) {
+game.load.image('wall', 'assets/wall.png');
+game.load.image('coin', 'assets/coin.png');
+game.load.image('enemy', 'assets/enemy.png');
+    },
+
+    create: function() {  
+        // Here we create the game
+        // Set the background color to blue
+game.stage.backgroundColor = '#3598db';
+// Variable to store the arrow key pressed
+this.cursor = game.input.keyboard.createCursorKeys();
+
+// Create the player in the middle of the game
+this.player = game.add.sprite(70, 100, 'player');
+
+// Add gravity to make it fall
+this.player.body.gravity.y = 600;
+
+// Start the Arcade physics system (for movements and collisions)
+game.physics.startSystem(Phaser.Physics.ARCADE);
+
+// Add the physics engine to all game objects
+game.world.enableBody = true;
+// Create 3 groups that will contain our objects
+this.walls = game.add.group();
+this.coins = game.add.group();
+this.enemies = game.add.group();
+
+// Design the level. x = wall, o = coin, ! = lava.
+var level = [
+    'xxxxxxxxxxxxxxxxxxxxxx',
+    '!         !          x',
+    '!                 o  x',
+    '!         o          x',
+    '!                    x',
+    '!     o   !    x     x',
+    'xxxxxxxxxxxxxxxx!!!!!x',
+];
+// Create the level by going through the array
+for (var i = 0; i < level.length; i++) {
+    for (var j = 0; j < level[i].length; j++) {
 
         // Create a wall and add it to the 'walls' group
         if (level[i][j] == 'x') {
@@ -64,27 +65,32 @@ window.onload = function() {
         }
     }
 }
-}
+    },
 
-function update() {
-    if (this.cursor.left.isDown) 
-        this.player.body.velocity.x = -200;
-    else if (this.cursor.right.isDown) 
-        this.player.body.velocity.x = 200;
-    else 
-       this.player.body.velocity.x = 0;
-   if (this.cursor.up.isDown && this.player.body.touching.down) 
+    update: function() {  
+        // Here we update the game 60 times per second
+        // Move the player when an arrow key is pressed
+if (this.cursor.left.isDown) 
+    this.player.body.velocity.x = -200;
+else if (this.cursor.right.isDown) 
+    this.player.body.velocity.x = 200;
+else 
+    this.player.body.velocity.x = 0;
+
+// Make the player jump if he is touching the ground
+if (this.cursor.up.isDown && this.player.body.touching.down) 
     this.player.body.velocity.y = -250;
-    // Make the player and the walls collide
-    game.physics.arcade.collide(this.player, this.walls);
+// Make the player and the walls collide
+game.physics.arcade.collide(this.player, this.walls);
 
 // Call the 'takeCoin' function when the player takes a coin
 game.physics.arcade.overlap(this.player, this.coins, this.takeCoin, null, this);
 
 // Call the 'restart' function when the player touches the enemy
 game.physics.arcade.overlap(this.player, this.enemies, this.restart, null, this);
-}
-// Function to kill a coin
+    },
+
+    // Function to kill a coin
 takeCoin: function(player, coin) {
     coin.kill();
 },
@@ -94,3 +100,8 @@ restart: function() {
     game.state.start('main');
 }
 };
+
+// Initialize the game and start our state
+var game = new Phaser.Game(500, 200);  
+game.state.add('main', mainState);  
+game.state.start('main');
